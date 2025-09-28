05e177e2800872fa9cf065c26a2d20eff0b3f249
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-09-28 16:30:30 +0100

    make ClapTrap attribute private and add method to Scavtrap class

--- ScavTrap.cpp
+++ ScavTrap.cpp
@@ -2,20 +2,29 @@
 #include <iostream>
 #include <string>
 
-ScavTrap::ScavTrap()
+ScavTrap::ScavTrap() : ClapTrap(), _isGateKeeper(false)
 {
     std::cout << "ScavTrap default Constrcutor called" << std::endl;
+    this->_health = 100;
+    this->_energy = 50;
+    this->_attack = 20;
 }
 
-ScavTrap::ScavTrap(const std::string &name) : ClapTrap(name)
+ScavTrap::ScavTrap(const std::string &name)
+    : ClapTrap(name),
+      _isGateKeeper(false)
 {
     std::cout << "ScavTrap Parameter Constrcutor called" << std::endl;
+    this->_health = 100;
+    this->_energy = 50;
+    this->_attack = 20;
 }
 
 ScavTrap::ScavTrap(const ScavTrap &other)
+    : ClapTrap(other),
+      _isGateKeeper(other._isGateKeeper)
 {
     std::cout << "ScavTrap Copy Constrcutor called" << std::endl;
-    ClapTrap::operator=(other);
 }
 
 ScavTrap::~ScavTrap()
@@ -29,10 +38,33 @@ ScavTrap &ScavTrap::operator=(const ScavTrap &other)
     if (this != &other)
         return *this;
     ClapTrap::operator=(other);
+    _isGateKeeper = other._isGateKeeper;
     return *this;
+}
+
+void ScavTrap::attack(const std::string &target)
+{
+    if (_health == 0)
+    {
+        std::cout << "ScavTrap " << _name
+                  << " is not able to attack : already dead" << std::endl;
+        return;
+    }
+    if (_energy == 0)
+    {
+        std::cout << "ScavTrap " << _name
+                  << " is not able to attack : no enery point" << std::endl;
+        return;
+    }
+    _energy -= 1;
+    std::cout << "ScavTrap " << _name << " attacks " << target << ", causing "
+              << _attack << " points of damage!" << std::endl;
 }
 
 void ScavTrap::guardGate()
 {
-    std::cout << "ScavTrap is now in Gate keeper mode" << std::endl;
+    if (_isGateKeeper)
+        std::cout << "ScavTrap is now in Gate keeper mode" << std::endl;
+    else
+        std::cout << "ScavTrap is now is not in Gate keeper mode" << std::endl;
 }
--- ScavTrap.hpp
+++ ScavTrap.hpp
@@ -4,12 +4,17 @@
 
 class ScavTrap : public ClapTrap
 {
+  private:
+    bool _isGateKeeper;
+
   public:
     ScavTrap();
     ScavTrap(const std::string &name);
     ScavTrap(const ScavTrap &other);
     ~ScavTrap();
+
     ScavTrap &operator=(const ScavTrap &other);
 
+    void attack(const std::string &target);
     void guardGate();
 };
