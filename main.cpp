0e03f169e0df3c80c825bf119411f84baa6b1cce
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-10-11 14:12:17 +0100

    add line to seperate between debug messages

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "ScavTrap.hpp"
+#include <iostream>
+#include <string>
 
 int main()
 {
@@ -8,12 +10,19 @@ int main()
 
     c = b;
 
+    std::cout << std::string(50,'-') << std::endl;
     a.attack("Jon");
     a.beRepaired(10);
     a.guardGate();
+    std::cout << std::string(50,'-') << std::endl;
+
     b.attack("jon");
     b.beRepaired(10);
     b.guardGate();
+
+    std::cout << std::string(50,'-') << std::endl;
+
     static_cast<ClapTrap>(b).attack("hello");
+    std::cout << std::string(50,'-') << std::endl;
     return 0;
 }
