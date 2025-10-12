edbbab571bef8cfa1851182e347b63d1418891af
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-10-12 18:34:52 +0100

    orginaize the main file

--- main.cpp
+++ main.cpp
@@ -1,28 +1,34 @@
 #include "ScavTrap.hpp"
+#include <cstring>
 #include <iostream>
 #include <string>
 
 int main()
 {
     ScavTrap a;
-    ScavTrap b("Bob");
+    ScavTrap b("Solaimane");
     ScavTrap c = a;
 
     c = b;
 
-    std::cout << std::string(50,'-') << std::endl;
+    std::cout << std::string(50, '-') << std::endl;
+    
     a.attack("Jon");
     a.beRepaired(10);
     a.guardGate();
-    std::cout << std::string(50,'-') << std::endl;
+
+    std::cout << std::string(50, '-') << std::endl;
 
     b.attack("jon");
     b.beRepaired(10);
     b.guardGate();
 
-    std::cout << std::string(50,'-') << std::endl;
+    std::cout << std::string(50, '-') << std::endl;
+    
+    c.attack("POP");
+    c.beRepaired(10);
+    b.guardGate();
 
-    static_cast<ClapTrap>(b).attack("hello");
-    std::cout << std::string(50,'-') << std::endl;
+    std::cout << std::string(50, '-') << std::endl;
     return 0;
 }
