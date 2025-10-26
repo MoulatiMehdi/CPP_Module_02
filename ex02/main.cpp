e49ef354257963e52ea3ce2ef19794de000bf02e
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-10-26 13:07:19 +0100

    add tests for FragTrap class functions and constructors

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,35 +1,18 @@
 #include "FragTrap.hpp"
-#include <iostream>
 #include <string>
 
 int main()
 {
-    std::string line(50, '-');
-
     FragTrap a("Bob");
     FragTrap b = a;
     FragTrap c;
 
     c = a;
 
-    std::cout << line << std::endl;
     a.highFivesGuys();
     a.beRepaired(50);
     a.attack("mike");
     a.takeDamage(50);
 
-    std::cout << line << std::endl;
-    b.highFivesGuys();
-    b.beRepaired(50);
-    b.attack("mike");
-    b.takeDamage(50);
-
-    std::cout << line << std::endl;
-    c.highFivesGuys();
-    c.beRepaired(50);
-    c.attack("mike");
-    c.takeDamage(50);
-
-    std::cout << line << std::endl;
     return 0;
 }
